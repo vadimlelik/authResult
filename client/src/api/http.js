46d745d994015231c5711cce9e2577626ccefdd7
vadimlelik <promiti90@gmail.com>
2024-07-01 13:10:56 +0300

    add  http interceptors

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,21 +1,43 @@
-import axios from "axios";
+import axios from 'axios'
 
-const baseURL = "https://jsonplaceholder.typicode.com/";
+const baseURL = 'http://localhost:3001/api/v1/'
 
-
+// http://localhost:3001/api/v1/auth/token
 const http = axios.create({
-    baseURL,
+	baseURL,
 })
 
-// http.interceptors.request.use(config => {})
+http.interceptors.request.use(
+	async (config) => {
+		/* expiresDate = localStorageService.getAccessTokenExpiresDate() */
+		/* refreshToken = localStorageService.getRefreshToken()  */
+		/* isExpired = refreshToken && expiresDate < Date.now()  */
+
+		if (true) {
+			const data = await http.get('/auth/token')
+			/* localStorageService.setTokens(data) */
+		}
+		// const accessToken = localStorageService.getAccessToken()
+		if (/*accessToken*/ true) {
+			config.headers = {
+				...config.headers,
+				// "Authorization": `Bearer ${/*accessToken*/}`
+			}
+		}
+		return config
+	},
+
+	(error) => {
+		return Promise.reject(error)
+	}
+)
 
 const httpClient = {
-    get: http.get,
-    post: http.post,
-    put: http.put,
-    delete: http.delete,
-    patch: http.patch
+	get: http.get,
+	post: http.post,
+	put: http.put,
+	delete: http.delete,
+	patch: http.patch,
 }
 
-
-export default httpClient;
+export default httpClient
