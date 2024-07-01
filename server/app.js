a16ae54fce6666c58a584e35389a707f52dd745b
vadimlelik <promiti90@gmail.com>
2024-07-01 13:00:47 +0300

    add route

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import authRoute from "./route/authRoute.js";
 
 
 const app = express();
@@ -12,8 +13,7 @@ app.use(bodyParser.json());
 dotenv.config()
 
 
-
-app.use('/api/v1/auth',authRoute);
+app.use('/api/v1/auth', authRoute);
 
 const start = async () => {
     try {
