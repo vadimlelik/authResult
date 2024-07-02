a85bedea9f98e8681a7d0a58d73e8384983b6c60
vadimlelik <promiti90@gmail.com>
2024-07-02 14:49:01 +0300

    add route users

---
+++ server/route/userRoute.js
@@ -0,0 +1,20 @@
+import {Router} from 'express';
+import authMiddleware from "../middleware/auth.middleware.js";
+import User from "../model/userModel.js";
+
+
+const router = new Router()
+
+router.get('/', authMiddleware, async (req, res) => {
+
+    try {
+        const allUsers = await User.find()
+        return res.status(200).json(allUsers)
+    } catch (e) {
+        console.log(e)
+    }
+
+})
+
+
+export default router;
