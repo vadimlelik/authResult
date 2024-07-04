776ac4fdd1953f2404f46be0f8357d8052aeaad9
vadimlelik <promiti90@gmail.com>
2024-07-04 11:06:29 +0300

    add:AppLoader

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import withAppStore from "./hoc/withAppStore";
 import {useRoutes} from "react-router-dom";
 import {routes} from "./config/routes";
+import AppLoader from "./hoc/AppLoader";
 
 function App() {
     const route = useRoutes(routes(true));
     return (
-        <div className="App">
-            {route}
-        </div>
+        <AppLoader>
+            <div className="App">
+                {route}
+            </div>
+        </AppLoader>
     );
 }
 
