3a4be728866f0a3fc11ce332ad43318335511d21
vadimlelik <promiti90@gmail.com>
2024-07-30 11:10:13 +0300

    add:loadPostList

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,10 +1,23 @@
-
+import {useSelector} from "react-redux";
+import {getAllPosts, getPostIsError, getPostIsLoading} from "../store/postSilce";
 
 const PostList = () => {
+
+	const isLoading = useSelector(getPostIsLoading);
+	const posts = useSelector(getAllPosts);
+	const error = useSelector(getPostIsError);
+
+	if(isLoading){
+		return <h1>...Loading</h1>
+	}
+	if(error){
+		return  <h1>{error}</h1>
+	}
 
 	return (
 		<div>
 			<h1>postList</h1>
+			{posts.map((post) => (<div key={post._id} >{post.content}</div>))}
 		</div>
 	)
 }
